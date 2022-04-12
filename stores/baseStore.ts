ef1e1532538a1fd6bd5c6a7dd7b93330fb7cc970
alice.telescoop <alice@telescoop.fr>
2022-04-12 17:13:01 +0200

    [*] adjust indents

--- stores/baseStore.ts
+++ stores/baseStore.ts
@@ -1,36 +1,36 @@
-import { defineStore } from "pinia"
-import { Base, Ressource } from "~/composables/types"
+import {defineStore} from "pinia"
+import {Base, Ressource} from "~/composables/types"
 
 export const useBaseStore = defineStore('base', {
-    state: () => ({
-        bases: <number[]>[],
-        basesById: <{ [key: number]: Base }>{},
-        ressourcesById: <{ [key: number]: Ressource }>{},
-    }),
-    actions: {
-        async refreshBases() {
-            const { data, error } = await useApiGet<Base[]>("bases")
-            if (!error.value) {
-                const bases = data.value
-                for (const base of bases) {
-                    this.basesById[base.id] = base
-                }
-                this.bases = bases.map(base => base.id)
-            }
-        },
-        async getBase(baseId: number) {
-            const { data, error } = await useApiGet<Base>(`bases/${baseId}`)
-            if (!error.value) {
-                const base = data.value
-                this.basesById[base.id] = base
-            }
-        },
-        async getRessource(ressourceId: number) {
-            const { data, error } = await useApiGet<Ressource>(`ressources/${ressourceId}`)
-            if (!error.value) {
-                const ressource = data.value
-                this.ressourcesById[ressource.id] = ressource
-            }
-        },
+  state: () => ({
+    bases: <number[]>[],
+    basesById: <{ [key: number]: Base }>{},
+    ressourcesById: <{ [key: number]: Ressource }>{},
+  }),
+  actions: {
+    async refreshBases() {
+      const {data, error} = await useApiGet<Base[]>("bases")
+      if (!error.value) {
+        const bases = data.value
+        for (const base of bases) {
+          this.basesById[base.id] = base
+        }
+        this.bases = bases.map(base => base.id)
+      }
     },
+    async getBase(baseId: number) {
+      const {data, error} = await useApiGet<Base>(`bases/${baseId}`)
+      if (!error.value) {
+        const base = data.value
+        this.basesById[base.id] = base
+      }
+    },
+    async getRessource(ressourceId: number) {
+      const {data, error} = await useApiGet<Ressource>(`ressources/${ressourceId}`)
+      if (!error.value) {
+        const ressource = data.value
+        this.ressourcesById[ressource.id] = ressource
+      }
+    },
+  },
 })
