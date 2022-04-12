34e2b3e227f186dba1c4ab05fe93d5cb5d132e1f
alice.telescoop <alice@telescoop.fr>
2022-04-12 12:57:16 +0200

    [*] adapt main layout to sketch

---
+++ components/layout/header.vue
@@ -0,0 +1,24 @@
+<template>
+<header class="fr-header" role="banner">
+    <div class="fr-header__body">
+        <div class="fr-container">
+            <div class="fr-header__body-row">
+              <DsfrLogo
+                :logoText="logoTitle"
+              />
+            </div>
+        </div>
+    </div>
+</header>
+
+</template>
+
+<script lang="ts" setup>
+const logoTitle = ["La plateforme de l'ANCT"]
+</script>
+
+<style lang="sass" scoped>
+.fr-header
+  box-shadow: none
+  border-bottom: var(--grey-925-125) 1px solid
+</style>
