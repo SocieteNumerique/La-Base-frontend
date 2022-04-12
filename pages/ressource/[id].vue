9f648e5a0ff2d13bb5cb9c5f815a1fdaf0014d2b
alice.telescoop <alice@telescoop.fr>
2022-04-12 12:57:45 +0200

    [*] draft of resource creation & edition

--- pages/ressource/[id].vue
+++ pages/ressource/[id].vue
@@ -1,25 +1,47 @@
 <template>
-    <div>
-        <h1>{{ ressource?.title }}</h1>
-        <p>{{ ressource?.content }}</p>
+  <div class="fr-col-2 fr-mt-5v">
+    <div v-for="(item, index) of menu" :key="index" class="fr-text-title--blue-france fr-text--lg fr-text--bold fr-pt-3v fr-pb-3v menu-item fr-pl-2w">
+      <div >
+        {{ item }}
+      </div>
     </div>
+  </div>
+
+  <div class="fr-col-6 fr-col-offset-1 fr-mt-12w">
+    <h6>Informations générales</h6>
+    <div class="fr-tags-group">
+      <Tag :selected="true" :tag-count="34" label="Sélectionné" />
+      <Tag :tag-count="34" label="Gratuit" />
+    </div>
+
+    <h6>Contenu</h6>
+  </div>
+
+  <div class="fr-col-3 fr-mt-12w">Vérification des doublons</div>
 </template>
 
 <script setup lang="ts">
 import { useBaseStore } from '~/stores/baseStore';
 import { Ressource } from '~/composables/types'
 
+definePageMeta({
+  title: "Nouvelle ressource",
+})
+
 const route = useRoute()
 const baseStore = useBaseStore()
 
+const resourceId = route.params.id as unknown as number
+const menu = ref<string[]>(["Informations", "Contenus(2)", "Label(s)"])
 
-if (!baseStore.ressourcesById[route.params.id]) {
-    baseStore.getRessource(route.params.id)
+
+if (!baseStore.ressourcesById[resourceId]) {
+    baseStore.getRessource(resourceId)
 }
 
 
 
-const ressource = computed(() => baseStore.ressourcesById[route.params.id])
+const ressource = computed(() => baseStore.ressourcesById[resourceId])
 // if (base.value?.isShort) {
 //     console.log("is short")
 //     baseStore.getBase(base.value.id)
@@ -33,3 +55,13 @@ const ressource = computed(() => baseStore.ressourcesById[route.params.id])
 // })
 
 </script>
+
+<style lang="sass" scoped>
+.menu-item
+  border-top: var(--grey-925-125) 1px solid
+  margin: 0
+  &:last-child
+      border-bottom: var(--grey-925-125) 1px solid
+
+
+</style>
---
+++ pages/ressource/new.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLayout name="default">
+    <template #header>
+      <h1>Nouvelle ressource</h1>
+      <div class="fr-mb-14w"/>
+    </template>
+
+    <div class="fr-col-2">
+      <h4 class="fr-text-title--blue-france">Pour commencer</h4>
+      <div class="fr-text--xs">Nous avons besoin de quelques informations essentielles pour créer la ressource.</div>
+    </div>
+
+    <div class="fr-col-6 fr-col-offset-1">
+      <h6>Informations générales</h6>
+
+      <h6>Contenu</h6>
+    </div>
+
+    <div class="fr-col-3">Vérification des doublons</div>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: false,
+  title: "Nouvelle ressource",
+})
+</script>
