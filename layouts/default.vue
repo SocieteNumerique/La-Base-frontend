47da53d4c3807333bda9b0857e88c4cad941c0b0
alice.telescoop <alice@telescoop.fr>
2022-04-25 09:29:21 +0200

    [*] header only if title is known

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,9 @@
 <template>
   <div data-fr-theme>
     <LayoutHeader/>
-    <!--    <div class="fr-container">
-          <DsfrNavigation :nav-items="navItems"></DsfrNavigation>
-        </div>-->
-
     <div class="fr-container fr-mt-4w">
       <slot name="header">
-        <div class="fr-grid-row">
+        <div v-if="route.meta.title" class="fr-grid-row">
           <h1>{{ route.meta.title }}</h1>
         </div>
       </slot>
@@ -16,7 +12,6 @@
         <slot/>
       </div>
     </div>
-
   <LayoutFooter />
   </div>
 </template>
