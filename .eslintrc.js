a27bf4f7a826572f2398e7fdcb7aa068437c4fe4
alice.telescoop <alice@telescoop.fr>
2022-04-26 16:48:26 +0200

    [*] linter configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,25 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+  },
   extends: [
-    '@nuxtjs/eslint-config-typescript'
-  ]
+    "plugin:@typescript-eslint/recommended",
+    "plugin:nuxt/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/script-setup-no-uses-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    semi: [2, "never"],
+  },
 }
