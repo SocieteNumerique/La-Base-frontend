4dbf4c52c9cf7d96b5f97d75f2bf616d2df4e174
alice.telescoop <alice@telescoop.fr>
2022-07-21 10:46:30 +0200

    feat (content edit): editing only stops if save was successfull

--- components/content/input/index.vue
+++ components/content/input/index.vue
@@ -17,7 +17,7 @@ import ContentInputFile from "~/components/content/input/file.vue"
 import { PropType } from "vue"
 import { Content } from "~/composables/types"
 
-defineEmits(["exit"])
+const emits = defineEmits(["exit"])
 defineProps({
   modelValue: { type: Object as PropType<Content>, required: true },
 })
@@ -35,6 +35,7 @@ async function saveContent() {
   const updatedContent = await updateContent(content.value)
   if (!updatedContent) return
   content.value = updatedContent!
+  emits("exit")
 }
 </script>
 
