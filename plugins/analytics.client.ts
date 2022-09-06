934b7e6b85ef7eaf483c48b93937f97f795fe8e8
Maxime Bellec <maxime@telescoop.fr>
2022-09-06 10:08:52 +0200

    [*] debug analytics

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -2,11 +2,13 @@ import VueMatomo from "vue-matomo"
 import { defineNuxtPlugin } from "#app"
 
 export default defineNuxtPlugin((nuxtApp) => {
+  console.log("### analytics", nuxtApp.vueApp, VueMatomo)
   nuxtApp.vueApp.use(VueMatomo, {
+    debug: true,
     host: "https://labaseanct.matomo.cloud",
     siteId: 1,
     requireConsent: false,
-    router: nuxtApp.router,
+    // router: nuxtApp.router,
     trackInitialView: true,
   })
 })
