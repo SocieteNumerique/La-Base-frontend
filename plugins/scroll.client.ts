81887b225ef00587cc34e03872908f9282b923e9
Maxime Bellec <maxime@telescoop.fr>
2022-09-06 09:45:27 +0200

    [*] scroll plugin: do not scroll to top when changing query args in the same page

--- plugins/scroll.client.ts
+++ plugins/scroll.client.ts
@@ -1,7 +1,10 @@
 import { defineNuxtPlugin } from "#app"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.$router.options.scrollBehavior = () => {
+  nuxtApp.$router.options.scrollBehavior = (to, from) => {
+    if (to.path == from.path) {
+      return
+    }
     return { left: 0, top: 0 }
   }
 })
