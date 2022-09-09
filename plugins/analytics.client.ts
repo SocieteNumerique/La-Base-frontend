d79f396251b8aa3f25c29e15bfa1e8a3b4f71a45
Maxime Bellec <maxbellec@gmail.com>
2022-09-09 10:22:30 +0200

    LB-58 Analytics (#22)
    Co-authored-by: Maxime Bellec <maxime@telescoop.fr>

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -1,14 +1,30 @@
-import VueMatomo from "vue-matomo"
 import { defineNuxtPlugin } from "#app"
 
 export default defineNuxtPlugin((nuxtApp) => {
-  console.log("### analytics", nuxtApp.vueApp, VueMatomo)
-  nuxtApp.vueApp.use(VueMatomo, {
-    debug: true,
-    host: "https://labaseanct.matomo.cloud",
-    siteId: 1,
-    requireConsent: false,
-    // router: nuxtApp.router,
-    trackInitialView: true,
+  if (process.env.NODE_ENV == "development") {
+    console.log("no tracking in developement")
+    return
+  }
+  // set up tracking
+  const _paq = (window._paq = window._paq || [])
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["trackPageView"])
+  _paq.push(["enableLinkTracking"])
+  _paq.push(["enableHeartBeatTimer"])
+  ;(function () {
+    const u = "https://labaseanct.matomo.cloud/"
+    _paq.push(["setTrackerUrl", u + "matomo.php"])
+    _paq.push(["setSiteId", "1"])
+    const d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0]
+    g.async = true
+    g.src = "//cdn.matomo.cloud/labaseanct.matomo.cloud/matomo.js"
+    s.parentNode.insertBefore(g, s)
+  })()
+
+  // track path changes
+  nuxtApp.$router.afterEach(() => {
+    _paq.push(["trackPageView"])
   })
 })
