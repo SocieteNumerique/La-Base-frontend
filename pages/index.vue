96d664a9b2ac4cddb4d56a2cac7c5589779da510
alice.telescoop <alice@telescoop.fr>
2022-09-20 13:05:47 +0200

    fix (icons): link arrow back

--- pages/index.vue
+++ pages/index.vue
@@ -23,8 +23,9 @@
           <div>
             <NuxtLink
               to="/a-propos"
-              class="no-underline no-append-ico fr-link fr-fi-arrow-right-line fr-link--icon-left fr-mt-2w"
+              class="no-underline no-append-ico fr-link fr-mt-2w"
             >
+              <VIcon name="ri-arrow-right-line" class="fr-mr-1w" scale="0.8 " />
               En savoir plus
             </NuxtLink>
           </div>
