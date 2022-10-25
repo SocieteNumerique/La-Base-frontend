ab6cf33efe747c1ced006c12c6f3243a1a4aee86
Maxime Bellec <maxime@telescoop.fr>
2022-10-25 16:57:02 +0200

    *

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/css/moine.sass", "@gouvminint/vue-dsfr/styles"],
+  css: ["@gouvminint/vue-dsfr/styles", "@/css/moine.sass"],
   head: {
     title: "La Base du numérique d'intérêt général",
     meta: [
