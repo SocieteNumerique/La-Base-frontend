9d9fae277b4ffab712d9445b558e67706d4d6a31
Maxime Bellec <maxime@telescoop.fr>
2022-10-28 20:47:48 +0200

    up

--- components/introTooltip.vue
+++ components/introTooltip.vue
@@ -2,6 +2,7 @@
   <div class="overlay">
     <div class="tooltip">
       <div class="header"></div>
+      <button class="close"></button>
     </div>
   </div>
 </template>
@@ -9,17 +10,9 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-.overlay {
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 1;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
 .tooltip .header {
   background: var(--background-action-high-blue-france);
   color: white;
+  text-align: center;
 }
 </style>
---
+++ stores/introStore.ts
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia"
+import { useApiGet } from "~/composables/api"
+
+type Tooltip = {
+  content: string
+  id: number
+  image?: string
+  position: "right" | "left" | "bottom" | "top"
+  title: string
+}
+
+export const useIntroStore = defineStore("intro", {
+  state: () => {
+    return { tooltipsById: <{ [key: number]: Tooltip }>{}, active: 0 }
+  },
+  actions: {
+    async getTooltips() {
+      const { data, error } = await useApiGet<Tooltip[]>("tooltips/")
+      if (!error.value) {
+        for (const tooltip of data.value!) {
+          this.tooltipsById[tooltip.id] = tooltip
+        }
+      }
+    },
+    hide() {
+      this.active = 0
+    },
+  },
+  getters: {
+    current: (state): Tooltip | null => {
+      if (!state.active) {
+        return null
+      }
+      return state.tooltipsById[state.active]
+    },
+  },
+})
