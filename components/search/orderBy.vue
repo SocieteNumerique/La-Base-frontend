43c417cd18e460226c30a604901b9e6d10f827eb
alice.telescoop <alice@telescoop.fr>
2022-11-22 16:10:13 +0100

    LB-269 feat (orderBy): also order by nb of pins
    + prepare ordering by rating

--- components/search/orderBy.vue
+++ components/search/orderBy.vue
@@ -29,5 +29,9 @@ const orderByOptions = [
   { text: "Du plus ancien au plus récent", value: "modified" },
   { text: "Du plus vu au moins vu", value: "-visit_count" },
   { text: "Du moins vu au plus vu", value: "visit_count" },
+  // { text: "Du mieux noté au moins bien noté", value: "-rating" },
+  // { text: "Du moins bien noté au mieux noté", value: "rating" },
+  { text: "Du plus enregistré au moins enregistré", value: "-pinned_count" },
+  { text: "Du moins enregistré au plus enregistré", value: "pinned_count" },
 ]
 </script>
