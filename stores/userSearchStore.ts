e8b9675b6595e42c687e944df38b51d3705e29e2
Maxime Bellec <maxime@telescoop.fr>
2022-12-14 10:13:25 +0100

    [*] refactor userSearchStore

--- stores/userSearchStore.ts
+++ stores/userSearchStore.ts
@@ -2,65 +2,64 @@ import { defineStore } from "pinia"
 import { useApiDelete, useApiGet, useApiPost } from "~/composables/api"
 import { UserSearch, UserSearchQuery } from "~/composables/types"
 
-export const useUserSearchStore = defineStore("userSearch", () => {
-  const userSearchById = ref<{ [id: number]: UserSearch }>({})
+type UserStoreState = {
+  userSearchById: { [id: number]: UserSearch }
+}
 
-  const addUserSearch = async (
-    query: UserSearchQuery,
-    name: string,
-    dataType: "resources" | "bases"
-  ) => {
-    const { data, error } = await useApiPost<UserSearch>(
-      `user_searches/`,
-      { query, name, dataType },
-      {},
-      "La recherche a bien été créée",
-      true
-    )
-    if (!error.value) {
-      const userSearch = data.value!
-      userSearchById.value[userSearch.id] = userSearch
-    }
-
-    return { data, error }
-  }
-
-  const deleteUserSearch = async (id: number) => {
-    const { error } = await useApiDelete(
-      `user_searches/${id}/`,
-      {},
-      "La recherche a bien été supprimée",
-      true
-    )
-    if (!error.value) {
-      delete userSearchById.value[id]
-    }
-  }
-
-  const getUserSearches = async () => {
-    const { data, error } = await useApiGet<UserSearch[]>("user_searches/")
-    if (!error.value && data.value!) {
-      for (const user_search of data.value!) {
-        userSearchById.value[user_search.id] = user_search
+export const useUserSearchStore = defineStore("userSearch", {
+  state: (): UserStoreState => ({
+    userSearchById: {},
+  }),
+  getters: {
+    nSearches: (state) => {
+      return Object.keys(state.userSearchById).length
+    },
+    searchesForDataType: (state) => {
+      return (dataType: "resources" | "bases") => {
+        return Object.values(state.userSearchById).filter(
+          (search: UserSearch) => search.dataType === dataType
+        )
       }
-    }
-  }
-
-  const nSearches = computed(() => Object.keys(userSearchById).length)
-  const searchesForDataType = computed(
-    () => (dataType: "resources" | "bases") => {
-      return Object.values(userSearchById.value).filter(
-        (search) => search.dataType === dataType
+    },
+  },
+  actions: {
+    async addUserSearch(
+      query: UserSearchQuery,
+      name: string,
+      dataType: "resources" | "bases"
+    ) {
+      const { data, error } = await useApiPost<UserSearch>(
+        `user_searches/`,
+        { query, name, dataType },
+        {},
+        "La recherche a bien été créée",
+        true
       )
-    }
-  )
+      if (!error.value) {
+        const userSearch = data.value!
+        this.userSearchById[userSearch.id] = userSearch
+      }
 
-  return {
-    addUserSearch,
-    deleteUserSearch,
-    getUserSearches,
-    nSearches,
-    searchesForDataType,
-    userSearchById,
-  }
+      return { data, error }
+    },
+    async deleteUserSearch(id: number) {
+      const { error } = await useApiDelete(
+        `user_searches/${id}/`,
+        {},
+        "La recherche a bien été supprimée",
+        true
+      )
+      if (!error.value) {
+        delete this.userSearchById[id]
+      }
+    },
+    async getUserSearches() {
+      const { data, error } = await useApiGet<UserSearch[]>("user_searches/")
+      if (!error.value && data.value!) {
+        for (const user_search of data.value!) {
+          this.userSearchById[user_search.id] = user_search
+        }
+      }
+    },
+  },
 })
