670d55b8251ceb8fd03c918669a36201f43eb596
Maxime Bellec <maxime@telescoop.fr>
2022-12-19 15:40:41 +0100

    fix: ReportSimpleModal: adapt motives wording for base/resources

--- components/report/simpleModal.vue
+++ components/report/simpleModal.vue
@@ -70,28 +70,52 @@ async function sendReport() {
   if (!error.value) emits("close")
 }
 
-const reportMotivesOptions = [
-  {
-    value: "",
-    text: "Sélectionnez une option",
-  },
-  {
-    text: "Le contenu est inapproprié",
-    value: "Le contenu est inapproprié",
-  },
-  {
-    text: "Le contenu est obsolète",
-    value: "Le contenu est obsolète",
-  },
-  {
-    text: "C'est le doublon d'une autre ressource",
-    value: "C'est le doublon d'une autre ressource",
-  },
-  {
-    text: "Il y a des erreurs",
-    value: "Il y a des erreurs",
-  },
-]
+const reportMotivesOptions =
+  props.instanceType === "Resource"
+    ? [
+        {
+          value: "",
+          text: "Sélectionnez une option",
+        },
+        {
+          text: "Le contenu est inapproprié",
+          value: "Le contenu est inapproprié",
+        },
+        {
+          text: "Le contenu est obsolète",
+          value: "Le contenu est obsolète",
+        },
+        {
+          text: "C'est le doublon d'une autre ressource",
+          value: "C'est le doublon d'une autre ressource",
+        },
+        {
+          text: "Il y a des erreurs",
+          value: "Il y a des erreurs",
+        },
+      ]
+    : [
+        {
+          value: "",
+          text: "Sélectionnez une option",
+        },
+        {
+          text: "Le contenu est inapproprié",
+          value: "Le contenu est inapproprié",
+        },
+        {
+          text: "Le contenu est obsolète",
+          value: "Le contenu est obsolète",
+        },
+        {
+          text: "C'est le doublon d'une autre base",
+          value: "C'est le doublon d'une autre base",
+        },
+        {
+          text: "Il y a des erreurs",
+          value: "Il y a des erreurs",
+        },
+      ]
 
 const actions = computed(() => [
   {
